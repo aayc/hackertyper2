3c486b0340c97dd17b6d9be9e67b15942182add9
Aaron Chan <aaron.y.chan64@gmail.com>
2020-05-09 16:57:49 -0400

    Allow backend to accept query params

--- functions/code.js
+++ functions/code.js
@@ -1,7 +1,10 @@
 const { getGithubFileInfo } = require("./github.js");
 
 exports.handler = async function(event, context, callback) {
+  let params = event.queryStringParameters
+  console.log("Params: ", params)
   let fileInfo = await getGithubFileInfo("aayc/hackertyper2", "gatsby-config.js")
+  // let fileInfo = await getGithubFileInfo(params.repository, params.file)
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(fileInfo)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Footer from "../components/footer"
@@ -9,11 +10,11 @@ import FileContentDisplay from "../components/fileContentDisplay"
 const IndexPage = () => {
   useEffect(() => {
     // examples of calling the API
-    fetch("/.netlify/functions/code")
+    fetch("/.netlify/functions/code?repository=aayc/hackertyper2&file=gatsby-config.js")
       .then(response => response.json())
       .then(code => console.log("Code: ", code))
 
-    fetch("/.netlify/functions/dir")
+    fetch("/.netlify/functions/dir?repository=aayc/hackertyper2")
       .then(response => response.json())
       .then(body => console.log("Dir", body))
   });
