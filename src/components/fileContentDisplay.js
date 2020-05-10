eda85a66cc3e09b59d54302cf046d863ea17c7dc
Brett Fisher <brett.fisher@endurance.com>
2020-05-09 18:48:54 -0600

    Merge

--- src/components/fileContentDisplay.js
+++ src/components/fileContentDisplay.js
@@ -17,9 +17,9 @@ const FileContentDisplay = () => {
     lineNumber: 1,
   })
   const [stats, setStats] = useState({ n_hacked_on: 0, n_lines_written: 0 })
-<<<<<<< HEAD
-  const [repository, setRepository] = useState("aayc/hackertyper2")
-  const [hackFile, setHackFile] = useState("gatsby-config.js")
+  const [repository, setRepository] = useState("torvalds/linux")
+  const [hackFile, setHackFile] = useState("cpu.c")
+  const [fileIdx, setFileIdx] = useState(0)
   const [availableRespositories, setAvailableRepositories] = useState([])
   const [showSearch, setShowSearch] = useState(false)
 
@@ -28,11 +28,6 @@ const FileContentDisplay = () => {
       setAvailableRepositories(repos)
     })
   }, [])
-=======
-  const [repository, setRepository] = useState("torvalds/linux")
-  const [hackFile, setHackFile] = useState("cpu.c")
-  const [fileIdx, setFileIdx] = useState(0)
->>>>>>> 175230534a61eb62bee7bf8119b54bc271759540
 
   useEffect(() => {
     setEditor(
@@ -71,7 +66,7 @@ const FileContentDisplay = () => {
   useEffect(() => {
     if (editor) {
       editor.setValue(fileContent.substring(0, srcIndex))
-      editor.setPosition({lineNumber: 10000, column: 4000})
+      editor.setPosition({ lineNumber: 10000, column: 4000 })
     }
   }, [editor, fileContent, srcIndex])
 
@@ -103,7 +98,7 @@ const FileContentDisplay = () => {
         </div>
       )}
       <div className="flex w-full h-12 bg-gray-800 text-white"></div>
-    {/* <div className="w-full h-full overflow-hidden">
+      {/* <div className="w-full h-full overflow-hidden">
       <div className="flex w-full h-12 vscode-bg text-white text-sidebar">
         {[hackFile].map((file, index) =>
           <button
